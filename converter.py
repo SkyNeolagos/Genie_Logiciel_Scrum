dd6310f598a63f97536b2a23e63a80c4f4941cc9
Loic <loic.guilmain@alumni.univ-avignon.fr>
2019-11-28 13:15:26 +0100

    Commit converter.py V1.1
    implementation fonction avec nom fichier

--- converter.py
+++ converter.py
@@ -13,5 +13,18 @@ def convertToTxt():
             os.rename(pos,replace)
             consoleCommand='pdftotext '+replace+' '+name+'.txt'
             os.system(consoleCommand)
+
+def convertTotxtWithName(fichier):
+    pathDirectory=os.path.dirname(os.path.abspath(__file__))
+    if fichier.endswith(".txt"):
+        os.remove(os.path.join(pathDirectory,fichier))
+    if fichier.endswith(".pdf"):
+        name=os.path.splitext(os.path.basename(fichier))[0]
+        replace=fichier.replace(' ','_')
+        os.rename(fichier,replace)
+        consoleCommand='pdftotext '+replace+' '+name+'.txt'
+        os.system(consoleCommand)
+
 if __name__ == '__main__':
     convertToTxt()
+    convertTotxtWithName("Alexandrov_2015_A_Modified_Tripartite_Model_for_Document_Representation_in_Internet_Sociology.pdf")
