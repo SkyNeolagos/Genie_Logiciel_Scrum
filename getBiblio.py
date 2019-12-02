34c8aa1733c55c11fb3bd203f8c009893d69e302
BastienGautier <45255217+BastienGautier@users.noreply.github.com>
2019-12-02 17:07:38 +0100

    Update getBiblio.py

--- getBiblio.py
+++ getBiblio.py
@@ -2,13 +2,15 @@ def getBiblio(mon_fichier):
 	mon_fichier = open(mon_fichier, "r")
 	textComplet = mon_fichier.read()
 	mon_fichier.close()
+	contenu=""
+	if "REFERENCES" in textComplet :
+		tmp,contenu = textComplet.rsplit("REFERENCES\n",1)
+	elif "References" in textComplet :
+		tmp,contenu = textComplet.rsplit("References\n",1)
 
-	if "REFERENCES" in contenu :
-		tmp,contenu=textComplet.rsplit("REFERENCES\n",1)
-	elif "References" in contenu :
-		tmp,contenu=textComplet.rsplit("References\n",1)
-
-	ff=contenu.find('')
+	if contenu=="":
+		return "References introuvables"
+	ff=contenu.find("")
 
 	if ff>0:
 		tmp3=contenu[ff:contenu.find('\n',ff)].strip()
@@ -18,6 +20,9 @@ def getBiblio(mon_fichier):
 			tmp2=tmp2[tmp2.find('\n'):].strip()
 			tmp2=tmp2[tmp2.find('\n'):].strip()
 			contenu=tmp1.strip()+"\n"+tmp2
+		else:
+			tmp1,tmp2=contenu.split("",1)
+			contenu=tmp1.strip()+"\n"+tmp2.strip()
 	contenu=contenu.strip()
 	contenu="References:\n\t"+contenu
 	return contenu
