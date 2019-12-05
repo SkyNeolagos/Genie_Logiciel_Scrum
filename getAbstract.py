8af30fd82b7c83a23c2df24fd635c27e4e6af5be
BastienGautier <45255217+BastienGautier@users.noreply.github.com>
2019-12-05 13:59:56 +0100

    Update getAbstract add "FF" problem

--- getAbstract.py
+++ getAbstract.py
@@ -1,3 +1,9 @@
+def isNum(string):
+	for char in string:
+		if char!="0" and char!="1" and char!="2" and char!="3" and char!="4" and char!="5" and char!="6" and char!="7" and char!="8" and char!="9" :
+			return False
+	return True
+
 def getAbstract(mon_fichier):
 	mon_fichier = open(mon_fichier, "r")
 	textComplet = mon_fichier.read()
@@ -24,6 +30,19 @@ def getAbstract(mon_fichier):
 
 	while contenu[len(contenu)-1]!="\n":
 		contenu=contenu[:len(contenu)-2]
+	
+	ff=contenu.find("")
+	if ff>0:
+		tmp3=contenu[ff:contenu.find('\n',ff)].strip()
+
+		if isNum(tmp3) or (tmp3!="" and textComplet.count(tmp3)>3):
+			tmp1,tmp2=contenu.split("",1)
+			tmp2=tmp2[tmp2.find('\n'):].strip()
+			tmp2=tmp2[tmp2.find('\n'):].strip()
+			contenu=tmp1.strip()+"\n"+tmp2
+		else:
+			tmp1,tmp2=contenu.split("",1)
+			contenu=tmp1.strip()+"\n"+tmp2.strip()
 
 	contenu=contenu.strip()
 	return contenu
