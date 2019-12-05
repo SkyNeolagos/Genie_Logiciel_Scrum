2c13a9ad7d41881e8ecc2343b9c6cb4da47ad34a
Loïc Guilmain <loic.guilmain@alumni.univ-avignon.fr>
2019-12-05 14:15:00 +0100

    Update main.py
    Verification si convertToText() stop ou resume le programme

--- main.py
+++ main.py
@@ -24,21 +24,20 @@ def rmFolderConvert():
 		os.system(consoleCommand)
         
 def main():
-	convertToTxt()
-	listFilesTxt=os.listdir("convertDossier")
-	##pathDirectory=os.path.dirname(os.path.abspath(__file__))+"/convertDossier"
-	createFolderFinal()
-	for pos in listFilesTxt:
-		if pos.endswith(".txt"):
-			chemin="convertDossier/"+pos
-			titre=getTitre(chemin)
-			abstract=getAbstract(chemin)
-			auteur=getAuthor(chemin)
-			biblio=getBiblio(chemin)
-			nom=os.path.splitext(os.path.basename(pos))[0]
-			writeConvert(getArgument(),nom,titre,auteur,abstract,biblio)
-	rmFolderConvert()			
-
+	if convertToTxt() == "resume":
+		listFilesTxt=os.listdir("convertDossier")
+		##pathDirectory=os.path.dirname(os.path.abspath(__file__))+"/convertDossier"
+		createFolderFinal()
+		for pos in listFilesTxt:
+			if pos.endswith(".txt"):
+				chemin="convertDossier/"+pos
+				titre=getTitre(chemin)
+				abstract=getAbstract(chemin)
+				auteur=getAuthor(chemin)
+				biblio=getBiblio(chemin)
+				nom=os.path.splitext(os.path.basename(pos))[0]
+				writeConvert(getArgument(),nom,titre,auteur,abstract,biblio)
+		rmFolderConvert()			
 
 if __name__ == '__main__':
     main()
