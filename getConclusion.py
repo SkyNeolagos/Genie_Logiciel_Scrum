4e11ebcbe3be69d7d75bd461f9137f4ca1617928
BastienGautier <45255217+BastienGautier@users.noreply.github.com>
2019-12-05 14:01:28 +0100

    Update getConclusion add "FF" problem

--- getConclusion.py
+++ getConclusion.py
@@ -1,3 +1,10 @@
+def isNum(string):
+	for char in string:
+		if char!="0" and char!="1" and char!="2" and char!="3" and char!="4" and char!="5" and char!="6" and char!="7" and char!="8" and char!="9" :
+			return False
+	return True
+
+
 def getConclusion(mon_fichier):
 	mon_fichier = open(mon_fichier, "r")
 	textComplet = mon_fichier.read()
@@ -25,7 +32,18 @@ def getConclusion(mon_fichier):
 	while contenu[0]!="\n":
 		contenu=contenu[1:]
 
-	contenu=contenu.strip()
-	contenu="Conclusion:\n\t"+contenu
+	ff=contenu.find("")
+	if ff>0:
+		tmp3=contenu[ff:contenu.find('\n',ff)].strip()
 
-	return contenu+"\n"
+		if isNum(tmp3) or (tmp3!="" and textComplet.count(tmp3)>3):
+			tmp1,tmp2=contenu.split("",1)
+			tmp2=tmp2[tmp2.find('\n'):].strip()
+			tmp2=tmp2[tmp2.find('\n'):].strip()
+			contenu=tmp1.strip()+"\n"+tmp2
+		else:
+			tmp1,tmp2=contenu.split("",1)
+			contenu=tmp1.strip()+"\n"+tmp2.strip()
+
+	contenu=contenu.strip()
+	return contenu
