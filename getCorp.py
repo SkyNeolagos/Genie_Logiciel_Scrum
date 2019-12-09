57760968b4d092a5463819dbbf8b857b055c7926
Spouchy <spouchy@hotmail.fr>
2019-12-09 15:38:57 +0100

    create GetCorp.py

---
+++ getCorp.py
@@ -0,0 +1,27 @@
+def getCorp(mon_fichier):
+	mon_fichier = open(mon_fichier, "r")
+	contenu = mon_fichier.read()
+	mon_fichier.close()
+
+	contenueIntro = getIntro(nom_fichier)
+	contenueIntro = contenueIntro[len(contenueIntro)-10:]
+
+	if contenueIntro in contenu :
+		tmp,contenu=contenu.split(contenueIntro,1)
+
+
+	if "DISCUSSION" in contenu :
+		contenu,tmp=contenu.split("DISCUSSION",1)
+	elif "Discussion" in contenu :
+		contenu,tmp=contenu.split("Discussion",1)
+	elif "Conclusions" in contenu:
+		contenu,tmp=contenu.split("Conclusions",1)
+	elif "Conclusion" in contenu :
+		contenu,tmp=contenu.split("Conclusion",1)
+
+
+	while contenu[len(contenu)-1]!="\n":
+		contenu=contenu[:len(contenu)-1]
+
+	contenu=contenu.strip()
+	return contenu
