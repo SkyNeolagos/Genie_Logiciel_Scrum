6ed57caf981ddff8adc253198d9e5f04d8abe811
Loïc Guilmain <loic.guilmain@alumni.univ-avignon.fr>
2020-01-06 13:22:52 +0100

    Add files via upload

--- getTitre.py
+++ getTitre.py
@@ -3,25 +3,25 @@ import re
 
 def getTitre(nomFichier):
 	titreSeparer = nomFichier.split("_")
-	debTitre = titreSeparer[2]
-	finTitreSeparer = titreSeparer[len(titreSeparer)-1].split(".")
-	finTitre = finTitreSeparer[0]
-
-
-	fichier = open(nomFichier, "r")
-	lignes = fichier.readlines()
-	fichier.close()
-	titreFinal=""
-	for i in range(0,len(lignes)):
-		if debTitre in lignes[i] and finTitre in lignes[i]:
-			titreFinal = lignes[i]
-			break
-		elif debTitre.lower() in lignes[i].lower():
-			titreFinal = lignes[i].rstrip()
-			y = i+1
-			while finTitre.lower() not in lignes[y].lower():
+	if len(titreSeparer)>2:
+		debTitre = titreSeparer[2]
+		finTitreSeparer = titreSeparer[len(titreSeparer)-1].split(".")
+		finTitre = finTitreSeparer[0]
+		fichier = open(nomFichier, "r")
+		lignes = fichier.readlines()
+		fichier.close()
+		titreFinal=""
+		for i in range(0,len(lignes)):
+			if debTitre in lignes[i] and finTitre in lignes[i]:
+				titreFinal = lignes[i]
+				break
+			elif debTitre.lower() in lignes[i].lower():
+				titreFinal = lignes[i].rstrip()
+				y = i+1
+				while finTitre.lower() not in lignes[y].lower():
+					titreFinal = titreFinal +" "+ lignes[y].rstrip()
+					y = y + 1
 				titreFinal = titreFinal +" "+ lignes[y].rstrip()
-				y = y + 1
-			titreFinal = titreFinal +" "+ lignes[y].rstrip()
-			break
-	return titreFinal
+				break
+		return titreFinal
+	return ' '.join(titreSeparer)
